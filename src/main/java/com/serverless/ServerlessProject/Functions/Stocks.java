c90cbc4adc2398978129b4c51ecefd93fc38b41d
El0H0Amine <106432432+El0H0Amine@users.noreply.github.com>
2023-01-19 19:02:40 +0100

    test github actions auto aws jar build

--- src/main/java/com/serverless/ServerlessProject/Functions/Stocks.java
+++ src/main/java/com/serverless/ServerlessProject/Functions/Stocks.java
@@ -39,8 +39,6 @@ public class Stocks {
             Double high = parseDouble(arg_list[5]);
             Double low = parseDouble(arg_list[6]);
             stockServices.create(id, name, ticker, open, close, high, low);
-
-
         };
 
     }
